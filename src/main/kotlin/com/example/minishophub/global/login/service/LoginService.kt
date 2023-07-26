410a70c10cad2ea03104e6f1caffaa6181129ee0
antaeho <an981022@naver.com>
2023-07-26 16:43:36 +0900

    feat: 로깅 추가

--- src/main/kotlin/com/example/minishophub/global/login/service/LoginService.kt
+++ src/main/kotlin/com/example/minishophub/global/login/service/LoginService.kt
@@ -1,6 +1,7 @@
 package com.example.minishophub.global.login.service
 
 import com.example.minishophub.domain.user.persistence.buyer.BuyerRepository
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -12,9 +13,16 @@ class LoginService(
     private val buyerRepository: BuyerRepository,
 ) : UserDetailsService {
 
+    private val log = KotlinLogging.logger { }
+
     override fun loadUserByUsername(email: String): UserDetails {
+
+        log.info { "LoginService - loadUserByUsername 시작" }
+
         val user = buyerRepository.findByEmail(email)
             ?: throw UsernameNotFoundException("해당 이메일이 존재하지 않습니다.")
+
+        log.info { "LoginService - loadUserByUsername 종료" }
 
         return User.builder()
             .username(user.email)
